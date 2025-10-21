c5022a358949b894394dca4d6c09c9d69e12593b
hasley2308g-sudo <hasley2308.g@gmail.com>
2025-10-21 12:35:11 -0700

    Add PrintFullName function and main execution

---
+++ prompt1.py
@@ -0,0 +1,10 @@
+def PrintFullName(x):
+   print(f"Full name: Ashley Garcia")
+   print(f'Favorite movie: Coraline')
+   print(f'Favorite food: Vodka pasta')
+
+def main():
+  PrintFullName(x)
+
+if __name__=="__main__":
+	main()
