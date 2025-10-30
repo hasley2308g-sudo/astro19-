7434e1d4863f828705b20fe45c56d36b8bd77a8c
hasley2308g-sudo <hasley2308.g@gmail.com>
2025-10-29 20:54:52 -0700

    codedemo.py

---
+++ codedemo.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import sys
+
+def main():
+	x = 0.0
+	for i in range(0, 20):
+		x = float(2 * i + 19)
+		print(f"the value of x is = {x}")
+
+if __name__ == "__main__":
+	main()
